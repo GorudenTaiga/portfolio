bdbc1e8376d4cba42a6c01a3a530074d63e2fa53
GorudenTaiga <goruden.taiga27@gmail.com>
2026-03-08 21:37:08 +0700

    chore: update Next.js to version 16.1.6 and adjust TypeScript configuration
    - Updated Next.js dependency from 15.2.4 to ^16.1.6 in package.json.
    - Modified tsconfig.json:
      - Changed JSX setting from "preserve" to "react-jsx".
      - Reformatted "lib" and "paths" for better readability.
      - Added ".next/dev/types/**/*.ts" to the include paths for TypeScript.

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,9 @@ const nextConfig: NextConfig = {
     MAIL_SERVICE: 'service_avw2dkv',
     MAIL_TEMPLATE: 'template_8lppxwf',
     MAIL_API: 'aaZZnIe5SaFRf95zZ'
+  },
+  images: {
+    remotePatterns: [new URL('https://upload.wikimedia.org/**'), new URL('https://rqbcrttxfhxmcaxiropg.supabase.co/storage/v1/object/public/storage/images/**')]
   }
 };
 
